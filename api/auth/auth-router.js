67883f99e88f79250b57c005e2a0cf2abbdb0cd5
Rlauckern <90931921+Rlauckern@users.noreply.github.com>
2022-04-25 14:13:19 -0700

    all tests passing

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,73 +1,96 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
-const router = require('express').Router()
+
+const bcrypt = require('bcryptjs');
+const Users = require('../users/users-model');
+const router = require('express').Router();
+const {
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength
+} = require('./auth-middleware');
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
-
   response:
   status 200
   {
     "user_id": 2,
     "username": "sue"
   }
-
   response on username taken:
   status 422
   {
     "message": "Username taken"
   }
-
   response on password three chars or less:
   status 422
   {
     "message": "Password must be longer than 3 chars"
   }
  */
-router.post('/register', (req, res, next) => {
-  res.json('register')
-})
+  router.post('/register', checkUsernameFree, checkPasswordLength, async (req, res, next) => {
+    try {
+      const user = { username: req.body.username, password: req.body.password };
+      const hash = bcrypt.hashSync(user.password, 12);
+      user.password = hash;
+      const result = await Users.add(user);
+      res.status(200).json(result);
+    } catch(err) {
+      res.status(500).json({ message: 'error adding user' });
+    }
+  })
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
   response:
   status 200
   {
     "message": "Welcome sue!"
   }
-
   response on invalid credentials:
   status 401
   {
     "message": "Invalid credentials"
   }
  */
-  router.post('/login', (req, res, next) => {
-    res.json('login')
+  router.post('/login', checkUsernameExists, (req, res, next) => {
+    const password = req.body.password;
+    if(bcrypt.compareSync(password, req.user.password)) {
+      req.session.user = req.user;
+      res.status(200).json({ message: `Welcome ${req.user.username}!` });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials'});
+    }
   })
-
 
 /**
   3 [GET] /api/auth/logout
-
   response for logged-in users:
   status 200
   {
     "message": "logged out"
   }
-
   response for not-logged-in users:
   status 200
   {
     "message": "no session"
   }
  */
-  router.get('/logout', (req, res, next) =>  {
-    res.json('logout')
+  router.get('/logout', (req, res, next) => {
+    if(req.session.user) {
+      req.session.destroy(err => {
+        if(err) {
+          res.status(500).json({ message: 'error while logging out'});
+        } else {
+          res.status(200).json({ message: 'logged out' });
+        }
+      })
+    } else {
+      res.status(200).json({ message: 'no session' });
+    }
   })
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
 
-
-module.exports = router
+module.exports = router;
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,13 +1,27 @@
+const router = require('express').Router();
+const { restricted } = require('../auth/auth-middleware');
+const Users = require('./users-model');
+
+router.get('/', restricted, (req, res, next) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    // .catch(next);
+    .catch(err => {
+      res.status(500).json({ message: 'error getting users' });
+    })
+})
+
+module.exports = router;
+
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
-const router = require('express').Router()
 
 
 /**
   [GET] /api/users
-
   This endpoint is RESTRICTED: only authenticated clients
   should have access.
-
   response:
   status 200
   [
@@ -17,7 +31,6 @@ const router = require('express').Router()
     },
     // etc
   ]
-
   response on non-authenticated:
   status 401
   {
@@ -25,10 +38,5 @@ const router = require('express').Router()
   }
  */
 
-router.get('/', (req, res, next) => {
-  res.json('users')
-})
-
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
-module.exports = router
